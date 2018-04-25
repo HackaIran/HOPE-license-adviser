4f6f2609a9c1544aa5c0df6b73ec47c70bf89707
pmzi <pouya.mozafar@gmail.com>
2018-04-26 01:22:51 +0430

    some more info on properties

--- src/data.property.js
+++ src/data.property.js
@@ -1,62 +1,62 @@
 module.exports = {
     commercialUse: {
         mark: 1,
-        description: "",
+        description: "This software and derivatives may be used for commercial purposes.",
         type:"Permission"
     },
     destribution: {
         mark: 1,
-        description: "",
+        description: "This software may be distributed.",
         type:"Permission"
     },
     modification: {
         mark: 1,
-        description: "",
+        description: "This software may be modified.",
         type:"Permission"
     },
     patentUse: {
         mark: 1,
-        description: "",
+        description: "This license provides an express grant of patent rights from contributors.",
         type:"Permission"
     },
     privateUse: {
         mark: 1,
-        description: "",
+        description: "Source can be used for private uses.",
         type:"Permission"
     },
     discloseSource: {
         mark: 1,
-        description: "",
+        description: "Source code must be made available when the software is distributed.",
         type:"Condition"        
     },
     licenseAndCopyRightNotice: {
         mark: 1,
-        description: "",
+        description: " A copy of the license and copyright notice must be included with the software",
         type:"Condition"     
     },
     sameLicense: {
         mark: 1,
-        description: "",
+        description: "Modifications must be released under the same license when distributing the software. In some cases a similar or related license may be used.",
         type:"Condition"     
     },
     stateChange: {
         mark: 1,
-        description: "",
+        description: "Changes made to the code must be documented.",
         type:"Condition"     
     },
     liability: {
         mark: 1,
-        description: "",
+        description: "This license includes a limitation of liability.",
         type:"Limitaion"     
     },
     tradeMarkUse: {
         mark: 1,
-        description: "",
+        description: "This license explicitly states that it does NOT grant trademark rights, even though licenses without such a statement probably do not grant any imlicit trademark rights.",
         type:"Limitaion"  
     },
     warranty: {
         mark: 1,
-        description: "",
+        description: "The license explicitly states that it does NOT provide any warranty.",
         type:"Limitaion"  
     }
 };
