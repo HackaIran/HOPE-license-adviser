c6393e5a4a11f3461d71bd08d4a69657cc734aa2
pmzi <pouya.mozafar@gmail.com>
2018-04-26 00:59:32 +0430

    data property added

--- src/data.property.js
+++ src/data.property.js
@@ -1,5 +1,49 @@
 module.exports = {
-    commercialUse:{
+    commercialUse: {
+        mark: 1,
+        description: ""
+    },
+    destribution: {
+        mark: 1,
+        description: ""
+    },
+    modification: {
+        mark: 1,
+        description: ""
+    },
+    patentUse: {
+        mark: 1,
+        description: ""
+    },
+    privateUse: {
+        mark: 1,
+        description: ""
+    },
+    discloseSource: {
+        mark: 1,
+        description: ""
+    },
+    licenseAndCopyRightNotice: {
+        mark: 1,
+        description: ""
+    },
+    sameLicense: {
+        mark: 1,
+        description: ""
+    },
+    stateChange: {
+        mark: 1,
+        description: ""
+    },
+    liability: {
+        mark: 1,
+        description: ""
+    },
+    tradeMarkUse: {
+        mark: 1,
+        description: ""
+    },
+    warranty: {
         mark: 1,
         description: ""
     }
