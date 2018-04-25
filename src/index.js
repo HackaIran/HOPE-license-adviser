8e02bb6a61e9368e248c35a981248d2508c63bb4
pmzi <pouya.mozafar@gmail.com>
2018-04-25 21:21:37 +0430

    methodes has been declared

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-export default function hopeLicenseAdviser() {
+const fs = require('fs')
+
+const data = require('./data.license.js');
+
+class LicenseAdviser {
+    constructor(licenseDatas = null) {
+      this.data = licenseDatas || data;
+    }
+
+    /**
+     *
+     * @param {Object} answers - An object contains some True/False fields
+     *
+     * @returns {Array} Array of proper licenses according to the answers will be returned
+     *
+     */
+
+    advice(answers = {}) {
+      return []
+    }
+
+    /**
+     *
+     * @param {String} name - The name of the license
+     *
+     * @returns {Object} An object containing properties and description of the license with the given name will be returned
+     *
+     */
+
+    getLicenseInfo(name) {
+      return {}
+    }
 
 }
+
+module.exports = LicenseAdviser
