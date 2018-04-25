7809b85f7b07c71798daec08724fdd762aa4b50a
pmzi <pouya.mozafar@gmail.com>
2018-04-25 20:46:38 +0430

    boilerplate has been made

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import hopeLicenseAdviser from '../src'
+
+describe('hopeLicenseAdviser', function () {
+  it('should exist', function () {
+    expect(hopeLicenseAdviser).to.exist
+  })
+})
